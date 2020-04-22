56236ae91c5ad092f21bbc62c6618954edb1e561
tarcisiooliveira <tarcisio@ymail.com>
2020-04-22 12:41:10 -0300

    rna-transcription solution

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -1,7 +1,10 @@
 defmodule RnaTranscription do
   @doc """
   Transcribes a character list representing DNA nucleotides to RNA
-
+  * `G` -> `C`
+  * `C` -> `G`
+  * `T` -> `A`
+  * `A` -> `U`
   ## Examples
 
   iex> RnaTranscription.to_rna('ACTG')
@@ -9,5 +12,13 @@ defmodule RnaTranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    [head | tail] = dna
+
+    case head do
+      ?G -> 'C' ++ if length(tail) > 0, do: to_rna(tail), else: ''
+      ?C -> 'G' ++ if length(tail) > 0, do: to_rna(tail), else: ''
+      ?T -> 'A' ++ if length(tail) > 0, do: to_rna(tail), else: ''
+      ?A -> 'U' ++ if length(tail) > 0, do: to_rna(tail), else: ''
+    end
   end
 end
