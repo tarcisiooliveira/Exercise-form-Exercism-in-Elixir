a21c55868b429e945adaa74431c586018442b278
tarcisiooliveira <tarcisio@ymail.com>
2020-04-22 20:13:24 -0300

    improving my code rna-transcription based on similar one using a standard library && creating and answering accumulate exercice

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -1,10 +1,13 @@
 defmodule RnaTranscription do
+  @rna %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
+  }
   @doc """
   Transcribes a character list representing DNA nucleotides to RNA
-  * `G` -> `C`
-  * `C` -> `G`
-  * `T` -> `A`
-  * `A` -> `U`
+
   ## Examples
 
   iex> RnaTranscription.to_rna('ACTG')
@@ -12,13 +15,16 @@ defmodule RnaTranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    [head | tail] = dna
+    dna
+    |> Enum.map(fn x -> @rna[x] end )
 
-    case head do
-      ?G -> 'C' ++ if length(tail) > 0, do: to_rna(tail), else: ''
-      ?C -> 'G' ++ if length(tail) > 0, do: to_rna(tail), else: ''
-      ?T -> 'A' ++ if length(tail) > 0, do: to_rna(tail), else: ''
-      ?A -> 'U' ++ if length(tail) > 0, do: to_rna(tail), else: ''
-    end
+    # bad answear
+    # [head | tail] = dna
+    # case head do
+    #   ?G -> 'C' ++ if length(tail) > 0, do: to_rna(tail), else: ''
+    #   ?C -> 'G' ++ if length(tail) > 0, do: to_rna(tail), else: ''
+    #   ?T -> 'A' ++ if length(tail) > 0, do: to_rna(tail), else: ''
+    #   ?A -> 'U' ++ if length(tail) > 0, do: to_rna(tail), else: ''
+    # end
   end
 end
