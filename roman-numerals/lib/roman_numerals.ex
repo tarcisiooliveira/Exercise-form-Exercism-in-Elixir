95b69458deef812eb668400578279781213df040
Tarcisio Oliveira <tarcisio@ymail.com>
2021-01-26 20:55:27 -0300

    refactor Roman Numerals based on a great solution starting sublist
    See you tomorrow

--- roman-numerals/lib/roman_numerals.ex
+++ roman-numerals/lib/roman_numerals.ex
@@ -1,75 +1,18 @@
 defmodule RomanNumerals do
-
-  @doc """
-  Convert the number to a roman number.
-   """
   @spec numeral(pos_integer) :: String.t()
-  def numeral(number) when number > 0 and number <= 3000 do
-    number
-    |> Integer.digits()
-    |> get_roman_number
-  end
+  def numeral(n) when n in 1000..3000, do: "M#{numeral(n-1000)}"
+  def numeral(n) when n in 900..999,   do: "CM#{numeral(n-900)}"
+  def numeral(n) when n in 500..899,   do: "D#{numeral(n-500)}"
+  def numeral(n) when n in 400..499,   do: "CD#{numeral(n-400)}"
+  def numeral(n) when n in 100..399,   do: "C#{numeral(n-100)}"
+  def numeral(n) when n in 90..99,     do: "XC#{numeral(n-90)}"
+  def numeral(n) when n in 50..89,     do: "L#{numeral(n-50)}"
+  def numeral(n) when n in 40.. 49,    do: "XL#{numeral(n-40)}"
+  def numeral(n) when n in 10..39,     do: "X#{numeral(n-10)}"
+  def numeral(9),                      do: "IX"
+  def numeral(n) when n in 5..8,       do: "V#{numeral(n-5)}"
+  def numeral(4),                      do: "IV"
+  def numeral(n) when n in 1..3,       do: "#{numeral(n-1)}I"
+  def numeral(0),                      do: ""
 
-  defp get_roman_number(list) do
-    reverse_list = Enum.reverse(list)
-
-    for x <- (length(list) - 1)..0 do
-      get_leter(x, Enum.at(reverse_list, x))
-    end
-    |> Enum.join("")
-  end
-
-  defp get_leter(index, number) when index == 3 do
-    case number do
-      0 -> ""
-      1 -> "M"
-      2 -> "MM"
-      3 -> "MMM"
-    end
-  end
-
-  defp get_leter(index, number) when index == 2 do
-    case number do
-      0 -> ""
-      1 -> "C"
-      2 -> "CC"
-      3 -> "CCC"
-      4 -> "CD"
-      5 -> "D"
-      6 -> "DC"
-      7 -> "DCC"
-      8 -> "DCCC"
-      9 -> "CM"
-    end
-  end
-
-  defp get_leter(index, number) when index == 1 do
-    case number do
-      0 -> ""
-      1 -> "X"
-      2 -> "XX"
-      3 -> "XXX"
-      4 -> "XL"
-      5 -> "L"
-      6 -> "LX"
-      7 -> "LXX"
-      8 -> "LXXX"
-      9 -> "XC"
-    end
-  end
-
-  defp get_leter(index, number) when index == 0 do
-    case number do
-      0 -> ""
-      1 -> "I"
-      2 -> "II"
-      3 -> "III"
-      4 -> "IV"
-      5 -> "V"
-      6 -> "VI"
-      7 -> "VII"
-      8 -> "VIII"
-      9 -> "IX"
-    end
-  end
 end
--- sublist/lib/sublist.ex
+++ sublist/lib/sublist.ex
@@ -4,5 +4,18 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
+
+  end
+  # defp sublist(a,b) do
+
+  # end
+  # defp equal(a,b) do
+
+  # end
+  # defp superlist(a,b) do
+
+  # end
+  defp nothing() do
+    :unequal
   end
 end
