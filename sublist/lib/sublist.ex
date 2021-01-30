dba50f18705d259589e219de832beb9cad1b0f9e
Tarcisio Oliveira <tarcisio@ymail.com>
2021-01-29 22:38:40 -0300

    sending sublist e downloading matching brackets

--- sublist/lib/sublist.ex
+++ sublist/lib/sublist.ex
@@ -3,19 +3,20 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
+
+  # def compare([], [nil]), do: :sublist
+  # def compare([nil], []), do: :superlist
+
   def compare(a, b) do
-
+    cond do
+      a == b -> :equal
+      sublist?(a, b) -> :sublist
+      sublist?(b, a) -> :superlist
+      true -> :unequal
+    end
   end
-  # defp sublist(a,b) do
 
-  # end
-  # defp equal(a,b) do
+  defp sublist?(_, []), do: false
 
-  # end
-  # defp superlist(a,b) do
-
-  # end
-  defp nothing() do
-    :unequal
-  end
+  defp sublist?(a, b), do: if(List.starts_with?(b, a), do: true, else: sublist?(a, tl(b)))
 end
