bc7ea1811dd15836d43ca3e9f6edf61f574369df
Tarcisio Oliveira <tarcisio@ymail.com>
2021-02-03 15:26:13 -0300

    solving triangle and downloading flatten arrau
    in Triangle problem i could be use used Enum.unique funcotin  the count of the same values to solve by other way.
    i could use Enum.order before call choose(), to make a better patter matching at line 13-17

--- triangle/lib/triangle.ex
+++ triangle/lib/triangle.ex
@@ -5,6 +5,14 @@ defmodule Triangle do
   Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
   """
   @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
-  def kind(a, b, c) do
-  end
+  def kind(a, b, c) when (a <= 0 or b <= 0 or c <= 0), do: {:error, "all side lengths must be positive"}
+
+  def kind(a, b, c) when ((a + b) >= c and (c + b) >= a), do: choose(a, b, c)
+  def kind(_a,_b,_c), do: {:error, "side lengths violate triangle inequality"}
+
+  defp choose(a, a, a) , do: {:ok, :equilateral}
+  defp choose(_a, b, b) , do: {:ok, :isosceles}
+  defp choose(a, _b, a), do: {:ok, :isosceles}
+  defp choose(a, a, _c), do: {:ok, :isosceles}
+  defp choose(_a, _b, _c), do: {:ok, :scalene}
 end
