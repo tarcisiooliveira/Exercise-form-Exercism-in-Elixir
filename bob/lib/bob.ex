9da0a7871896e82f84583aacda2692fbc47513c0
Tarcisio Oliveira <tarcisio@ymail.com>
2021-02-05 19:18:41 -0300

    Bob completed, Refactoring triangle and downloading Anagram
    Triangle has changed the function name to do_kind by the mentor solicitation

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,42 +1,36 @@
 defmodule Bob do
   def hey(input) do
     input = String.replace(input, ~r/[\t\:\)\(\)\n\r ]/, "")
-    # |>IO.puts()
-    # |> String.codepoints()
-    # |> IO.inspect()
 
     cond do
       input == "" ->
         "Fine. Be that way!"
 
+      String.match?(input, ~r/^\s/) == true ->
+        "Fine. Be that way!"
+
       String.ends_with?(input, "?") and
-      String.match?(input, ~r/\p{L}/)==true and
-      input == String.upcase(input) ->
+        String.match?(input, ~r/\p{L}/) == true and
+          input == String.upcase(input) ->
         "Calm down, I know what I'm doing!"
 
-
-      String.ends_with?(input,"?") and
-      replace_digits(input)|>String.length()>1 and
-      contains_downcase(input)==true ->
-      "Sure."
-
-      replace_digits(input)
-      |> contains_downcase()==false and
-      contains_upcase(input)==true->
+      String.match?(input, ~r/\p{L}/) == true and
+      input == String.upcase(input) ->
         "Whoa, chill out!"
+
+      String.ends_with?(input, "?") ->
+        "Sure."
 
       true ->
         "Whatever."
     end
   end
 
-  defp contains_downcase(input), do: String.match?(input, ~r/[[:lower:]]/)
+  # defp contains_downcase(input), do: String.match?(input, ~r/[[:lower:]]/)
 
-  defp contains_digit(input),
-    do: String.match?(input, ~r/["1","2","3","4","5",""6","7","8","9","0"]/)
+  # defp contains_digit(input),
+  #   do: String.match?(input, ~r/["1","2","3","4","5",""6","7","8","9","0"]/)
 
-  defp contains_upcase(input), do: String.match?(input, ~r/[[:upper:]]/)
-  defp replace_digits(input), do: String.replace(input, ~r/[[:digit:]\t\:\)\(\)\n\r\! ]/, "")
-
-
+  # defp contains_upcase(input), do: String.match?(input, ~r/[[:upper:]]/)
+  # defp replace_digits(input), do: String.replace(input, ~r/[[:digit:]\t\:\)\(\)\n\r\! ]/, "")
 end
--- triangle/lib/triangle.ex
+++ triangle/lib/triangle.ex
@@ -7,12 +7,12 @@ defmodule Triangle do
   @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
   def kind(a, b, c) when (a <= 0 or b <= 0 or c <= 0), do: {:error, "all side lengths must be positive"}
 
-  def kind(a, b, c) when ((a + b) >= c and (c + b) >= a), do: choose(a, b, c)
+  def kind(a, b, c) when ((a + b) >= c and (c + b) >= a), do: do_kind(a, b, c)
   def kind(_a,_b,_c), do: {:error, "side lengths violate triangle inequality"}
 
-  defp choose(a, a, a) , do: {:ok, :equilateral}
-  defp choose(_a, b, b) , do: {:ok, :isosceles}
-  defp choose(a, _b, a), do: {:ok, :isosceles}
-  defp choose(a, a, _c), do: {:ok, :isosceles}
-  defp choose(_a, _b, _c), do: {:ok, :scalene}
+  defp do_kind(a, a, a) , do: {:ok, :equilateral}
+  defp do_kind(_a, b, b) , do: {:ok, :isosceles}
+  defp do_kind(a, _b, a), do: {:ok, :isosceles}
+  defp do_kind(a, a, _c), do: {:ok, :isosceles}
+  defp do_kind(_a, _b, _c), do: {:ok, :scalene}
 end
