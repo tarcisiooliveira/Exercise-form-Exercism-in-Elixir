ea365f8b39c506f509d24826e04aa3c917c37071
tarcisiooliveira <tarcisio@ymail.com>
2021-02-10 22:31:30 -0300

    refactoring

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,6 +1,6 @@
 defmodule Bob do
   def hey(input) do
-    input = String.replace(input, ~r/[\t\:\)\(\)\n\r ]/, "")
+    input = String.trim(input)
 
     cond do
       input == "" ->
@@ -10,12 +10,12 @@ defmodule Bob do
         "Fine. Be that way!"
 
       String.ends_with?(input, "?") and
-        String.match?(input, ~r/\p{L}/) == true and
+        String.downcase(input) != input and
           input == String.upcase(input) ->
         "Calm down, I know what I'm doing!"
 
-      String.match?(input, ~r/\p{L}/) == true and
-      input == String.upcase(input) ->
+      String.downcase(input) != input and
+          input == String.upcase(input) ->
         "Whoa, chill out!"
 
       String.ends_with?(input, "?") ->
@@ -25,12 +25,4 @@ defmodule Bob do
         "Whatever."
     end
   end
-
-  # defp contains_downcase(input), do: String.match?(input, ~r/[[:lower:]]/)
-
-  # defp contains_digit(input),
-  #   do: String.match?(input, ~r/["1","2","3","4","5",""6","7","8","9","0"]/)
-
-  # defp contains_upcase(input), do: String.match?(input, ~r/[[:upper:]]/)
-  # defp replace_digits(input), do: String.replace(input, ~r/[[:digit:]\t\:\)\(\)\n\r\! ]/, "")
 end
